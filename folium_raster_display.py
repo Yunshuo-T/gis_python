e674cf073b2c2f7a6dbc073c2f8095b847fcc3dd
TYS <551835@mail.muni.cz>
2025-05-13 10:02:06 +0200

    Update folium_raster_display.py

--- folium_raster_display.py
+++ folium_raster_display.py
@@ -1,11 +1,13 @@
-def folium_raster_display(file_path,file_name,tile_name = "OpenstreetMap",control_scale=False):
+def folium_raster_display(file_path,file_name,tile_name = "OpenstreetMap",control_scale=False,zoom_start=10):
     """
     Display a raster file using "folium.raster_layers.ImageOverlay" in Folium.
+    Attention: tha raster file's bounds should be in lat and lon format, otherwise it has to transfrom in advance.
     Args:
         file_path (str): Path to the raster file.
         file_name(str): The name of the raster file.
-        tile_name (str): Name of the tile layer to use. Default is "OpenstreetMap".
-        control_scale (bool): Whether to show the scale control. Default is False.
+        tile_name (str): Default is "OpenstreetMap". Name of the tile layer to use. 
+        control_scale (bool): Default is False. Whether to show the scale control. 
+        zoom_start(int):  int, default 10. Initial zoom level for the map.
     Returns:
         folium.Map: Folium map with the raster overlay.
     Depencies:
@@ -17,27 +19,21 @@ def folium_raster_display(file_path,file_name,tile_name = "OpenstreetMap",contro
     import folium
     from PIL import Image
     import rasterio
-    import numpy as np
     import folium.raster_layers
     # Open the raster file using rasterio
     with rasterio.open(file_path) as src:
         
-        # Read the raster data
-        bands =  src.read() #Read all bands
-        bands = np.moveaxis(bands, 0, -1) #Move the first axis to the last axis
-
         # Get the bounds of the raster
         bounds = src.bounds
         converted_bounds = [[bounds.bottom, bounds.left], [bounds.top, bounds.right]]
         # Convert the bands to a PIL image
-        image = Image.fromarray(bands)
-
+        image = Image.open(file_path)
         # Create a Folium map centered on the raster bounds
         m = folium.Map(
             location=[
                 (bounds.top + bounds.bottom) / 2, 
                 (bounds.left + bounds.right) / 2], 
-            zoom_start=12,
+            zoom_start=zoom_start,
             tiles=tile_name,
             control_scale=control_scale,
         )

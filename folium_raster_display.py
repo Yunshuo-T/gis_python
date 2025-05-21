db7f229e506dd0c10871be7fb944b3fad543b59f
TYS <551835@mail.muni.cz>
2025-05-21 14:23:02 +0200

    Update folium_raster_display.py
    Add control to create layer with base map or only layer itself

--- folium_raster_display.py
+++ folium_raster_display.py
@@ -1,13 +1,15 @@
-def folium_raster_display(file_path,file_name,tile_name = "OpenstreetMap",control_scale=False,zoom_start=10):
+def folium_raster_display(
+    file_path, file_name, tile_name="OpenstreetMap", control_scale=False, zoom_start=10, m=True
+):
     """
     Display a raster file using "folium.raster_layers.ImageOverlay" in Folium.
-    Attention: tha raster file's bounds should be in lat and lon format, otherwise it has to transfrom in advance.
     Args:
         file_path (str): Path to the raster file.
         file_name(str): The name of the raster file.
-        tile_name (str): Default is "OpenstreetMap". Name of the tile layer to use. 
-        control_scale (bool): Default is False. Whether to show the scale control. 
+        tile_name (str): Default is "OpenstreetMap". Name of the tile layer to use.
+        control_scale (bool): Default is False. Whether to show the scale control.
         zoom_start(int):  int, default 10. Initial zoom level for the map.
+        m(bool): Defualt is True. Whether to Generate a base map.
     Returns:
         folium.Map: Folium map with the raster overlay.
     Depencies:
@@ -20,30 +22,37 @@ def folium_raster_display(file_path,file_name,tile_name = "OpenstreetMap",contro
     from PIL import Image
     import rasterio
     import folium.raster_layers
+
     # Open the raster file using rasterio
     with rasterio.open(file_path) as src:
-        
+
         # Get the bounds of the raster
         bounds = src.bounds
         converted_bounds = [[bounds.bottom, bounds.left], [bounds.top, bounds.right]]
         # Convert the bands to a PIL image
         image = Image.open(file_path)
         # Create a Folium map centered on the raster bounds
-        m = folium.Map(
-            location=[
-                (bounds.top + bounds.bottom) / 2, 
-                (bounds.left + bounds.right) / 2], 
-            zoom_start=zoom_start,
-            tiles=tile_name,
-            control_scale=control_scale,
-        )
+
         # Add the raster overlay to the map
-        folium.raster_layers.ImageOverlay(
+        layer = folium.raster_layers.ImageOverlay(
             image=image,
             bounds=converted_bounds,
             mercator_project=True,
             overlay=True,
-            name = str(file_name),
-        ).add_to(m)
-        folium.LayerControl().add_to(m)
-        return m
+            name=str(file_name),
+        )
+
+        if m:
+            m = folium.Map(
+                location=[
+                    (bounds.top + bounds.bottom) / 2,
+                    (bounds.left + bounds.right) / 2,
+                ],
+                zoom_start=zoom_start,
+                tiles=tile_name,
+                control_scale=control_scale,
+            )
+            layer.add_to(m)
+            return  m
+        else:
+            return layer
